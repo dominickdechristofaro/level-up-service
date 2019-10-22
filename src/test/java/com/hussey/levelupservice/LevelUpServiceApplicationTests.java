84683ecfd55cdd67ebcf60fce41ea461faaaadb1
Dominick <dominick.dechristofaro@gmail.com>
2019-10-22 11:59:19 -0400

    initial commit for level-up-service

---
+++ src/test/java/com/hussey/levelupservice/LevelUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussey.levelupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
